fc9f946fc7d5f446ce6abd17d6c904ccf5ea1d4b
Madison Scott-Clary <makyo@drab-makyo.com>
2024-05-04 15:34:34 -0700

    Dark mode

---
+++ static/js/theme.js
@@ -0,0 +1,30 @@
+// Theme switcher from CSS-Tricks. It was there for the taking.
+
+// Select the button
+const themeToggle = document.querySelector(".theme-toggle");
+
+// Check for dark mode preference at the OS level
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+// Set the body class and button contents according to the theme, defaulting to light
+const currentTheme = localStorage.getItem("theme") || (prefersDarkScheme.matches ? "dark" : "light");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-mode");
+  themeToggle.innerText = "☀";
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-mode");
+  themeToggle.innerText = "☾"
+}
+
+// A bit redundant, but in the cases where the theme has not yet been set, we want make sure it finds its way into localStorage.
+localStorage.setItem("theme", currentTheme);
+
+// Listen for a click on the button 
+themeToggle.addEventListener("click", function() {
+  document.body.classList.toggle("light-mode");
+  document.body.classList.toggle("dark-mode");
+  const theme = localStorage.getItem("theme") == "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", theme);
+  themeToggle.innerText = theme == "light" ? "☾" : "☀" ;
+});
